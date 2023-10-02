692bffc54ac37e73aed2a60a110ba20585577851
gabrielseibel1 <gabrielseibel1@gmail.com>
2023-10-02 19:15:53 -0300

    Solve leetcode 206

---
+++ src/lc206.rs
@@ -0,0 +1,34 @@
+use log::warn;
+
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode {
+            next: None,
+            val,
+        }
+    }
+}
+
+pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    let mut curr: Option<Box<ListNode>> = None;
+    let mut next: Option<Box<ListNode>> = head;
+    loop {
+        if next.is_none() {
+            break
+        }
+        let mut next_node: Box<ListNode> = next.take().unwrap();
+        let fwd = next_node.next.take();
+        next_node.next = curr.take();
+        curr = Some(next_node);
+        next = fwd;
+    }
+    curr
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,4 @@ pub mod lc217;
 pub mod lc242;
 pub mod lc74;
 pub mod lc143;
+pub mod lc206;
